ef9a823d48e2ac9790baee8760be2bf4edb41511
Emma Gerigscott <elgerig@gmail.com>
2023-05-07 20:22:30 -0700

    add form and postcard path to submit user info

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -17,9 +17,19 @@ namespace FriendLetter.Controllers
       LetterVariable myLetterVariable = new LetterVariable();
       myLetterVariable.Recipient = "Lina";
       myLetterVariable.Sender = "Jasmine";
-      return View(myLetterVariable); }
+      return View(myLetterVariable);
+    }
 
-      [Route"/form"]
-      public ActionResult Form() { return View(); }
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+      
+    [Route("/postcard")]
+    public ActionResult Postcard(string recipient, string sender) 
+    { 
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = recipient;
+      myLetterVariable.Sender = sender;
+      return View(myLetterVariable);
+    }
   }
 }
